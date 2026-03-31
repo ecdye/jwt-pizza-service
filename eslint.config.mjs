b8f00b2c7b40a2175265be0975fe71b4a455b287
Ethan Dye <mrtops03@gmail.com>
2026-03-31 17:03:24 -0600

    Fix linting
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { files: ['src/**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.node } },
   { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
